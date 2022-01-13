f67d8bc8f960ee3883441f57f1f85d13546bb371
“Behanzin <sarah.behanzin@edu.esiee.fr>
2022-01-13 11:49:06 +0100

    fonctions

--- main.py
+++ main.py
@@ -17,10 +17,25 @@ def fontion_correspondance(mot_entre):
     if mot_entre=="ger" or mot_entre=="frg" or mot_entre=="république fédérale d'allemagne":
         mot="ger"
     if mot_entre=="russie" or mot_entre=="urs":
-        mot="russie"
+        mot="russia"
     return mot
 
 def fonction_pays(nom_pays):
     nom_pays=nom_pays.lower()
-    nom_final=nom_pays[0:2]
+    nom_final=nom_pays[0:3]
     return nom_final
+
+def traduction_pays(nom_pays):
+    nom_pays=nom_pays.lower()
+    if nom_pays=="allemagne":
+        pays_final="germany"
+    if nom_pays=="angleterre":
+        pays_final="england"
+    
+    return pays_final
+
+def main():
+    return None
+
+if __name__ == '__main__':
+    main()
